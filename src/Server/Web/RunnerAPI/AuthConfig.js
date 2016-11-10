47c87be6edcd0ccd5c9e6873b08260f767a4dad2
Nathan Mentley <nathanmentley@gmail.com>
2016-11-09 23:03:16 -0800

    bug fix. In RunnerAPI Auth.

--- src/Server/Web/RunnerAPI/AuthConfig.js
+++ src/Server/Web/RunnerAPI/AuthConfig.js
@@ -1,39 +1,41 @@
 (function() {;
-	module.exports = function(req, res, next) {
-		var originDomain = req.get('origin').replace("http://", "");
+	module.exports = function(permission) {
+		return function(req, res, next) {
+			var originDomain = req.get('origin').replace("http://", "");
 
-		Attollo.Services.Site.GetSite({}, originDomain)
-		.then(function (sites) {
-			var site = sites.first();
+			Attollo.Services.Site.GetSite({}, originDomain)
+			.then(function (sites) {
+				var site = sites.first();
 
-			Attollo.Services.Site.GetCurrentSiteVersion({}, site)
-			.then(function (siteVersions) {
-				var siteVersion = siteVersions.first();
+				Attollo.Services.Site.GetCurrentSiteVersion({}, site)
+				.then(function (siteVersions) {
+					var siteVersion = siteVersions.first();
 
-				if(site && siteVersion) {
-					req.AuthContext = {
-						ClientID: site.get('clientid'),
-						SiteID: site.get('id'),
-						SiteVersionID: siteVersion.get('id')
-					};
+					if(site && siteVersion) {
+						req.AuthContext = {
+							ClientID: site.get('clientid'),
+							SiteID: site.get('id'),
+							SiteVersionID: siteVersion.get('id')
+						};
 
-					next();
-				} else {
+						next();
+					} else {
+						res.statusCode = 403;
+						res.setHeader('WWW-Authenticate', 'Basic realm="Attollo"');
+						res.end('Forbidden');
+					}
+				})
+				.catch(function (err) {
 					res.statusCode = 403;
 					res.setHeader('WWW-Authenticate', 'Basic realm="Attollo"');
 					res.end('Forbidden');
-				}
+				});
 			})
-			.catch(function (err) {
+			.catch(function(err) {
 				res.statusCode = 403;
 				res.setHeader('WWW-Authenticate', 'Basic realm="Attollo"');
 				res.end('Forbidden');
 			});
-		})
-		.catch(function(err) {
-			res.statusCode = 403;
-			res.setHeader('WWW-Authenticate', 'Basic realm="Attollo"');
-			res.end('Forbidden');
-		});
+		};
 	};
 })(); 
